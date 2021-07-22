e95a19994ba597f9814883869421d05fa6ff3292
giofile <77681874+giofile@users.noreply.github.com>
2021-07-22 14:41:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const makePizza = function(){
+    console.log("deeg rollen");
+    console.log("tomatensaus erop");
+    console.log("kaas erop");
+    console.log("paprike erop");
+    console.log("over verwarmen");
+    console.log("pizza in de oven");
+    console.log("uit de oven een lekkere pizza");
+}
+
+makePizza()
+makePizza()
+makePizza()
+
+const sushiMaken = function(){
+    console.log("in stukken delen");
+    console.log("rijst toevoegen");
+    console.log("zeewier om heen wikkelen");
+   
+
+}
+
+makePizza()
+makePizza()
+sushiMaken()
+sushiMaken()
+sushiMaken()
